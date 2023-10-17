894a95daee0307a09f2d3e0ede75b0ac2dabf061
e1114566@u.nus.edu <e1114566@u.nus.edu>
2023-10-18 01:56:46 +0800

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const express = require('express')
+const {engine} = require ('express-handlebars')
 
+//Port to listen to 
+const PORT = parseInt(process.env.PORT)||3000
+
+//Create an instance of express
+const app = express()
+
+
+app.use(express.static(__dirname + '/static'))
+
+app.listen(PORT, () => {
+    console.info(`Application started on port ${PORT} at ${new Date()}`);
+});
